27fbcdb14fc3497b2e295a30a2efdd5c6769166e
Noor <noorfarooqy@gmail.com>
2019-02-10 14:43:54 +0800

    Enclose in container policies pages

--- resources/views/privacy/cookies.blade.php
+++ resources/views/privacy/cookies.blade.php
@@ -7,90 +7,93 @@
 
 @section ("content")
 
-<div class="row">
-	<div class="col-md-2 col-lg-2"></div>
-	<div class="col-md-8 col-lg-8">
-		<div class="row">
+<div class="container" style="font-size:Times New Roman">
+	<div class="row">
+		<div class="col-md-2 col-lg-2"></div>
+		<div class="col-md-8 col-lg-8">
+			<div class="row">
 
-			@php
+				@php
 
-			$cookies = simplexml_load_file("xml/cookies.xml");
-			@endphp
+				$cookies = simplexml_load_file("xml/cookies.xml");
+				@endphp
 
-			<h1>
-				{{$cookies->header}}
-			</h1>
-			<h3>{{$cookies->updatetime}}</h3>
-			<p>
-				{{$cookies->topbody->div1}}<br>
-			</p>
-			<p>
-				{{$cookies->topbody->div2}}
-			</p>
-			<p>
-				<h2>{{$cookies->whararecookies->h}}</h2>
-				{{$cookies->whararecookies->div1}}
-				<p>{{$cookies->whararecookies->div2}}</p>
-			</p>
-			<p>
-				<h2>{{$cookies->howweuse->h}}</h2>
-				{{$cookies->howweuse->div1}}
-				<ol>
-					{{$cookies->howweuse->subh}}
-					<li>{{$cookies->howweuse->li1}}</li>
-					<li>{{$cookies->howweuse->li2}}</li>
-					<li>{{$cookies->howweuse->li3}}</li>
-				</ol>
-			</p>
-			<p>
-				<h2>{{$cookies->choice->h}}</h2>
+				<h1>
+					{{$cookies->header}}
+				</h1>
+				<h3>{{$cookies->updatetime}}</h3>
 				<p>
-					{{$cookies->choice->div1}}
+					{{$cookies->topbody->div1}}<br>
 				</p>
 				<p>
-					{{$cookies->choice->div2}}
+					{{$cookies->topbody->div2}}
 				</p>
 				<p>
-					{{$cookies->choice->subh}}
-				</p>
-				<ol>
-					<li>{{$cookies->choice->li1}}</li>
-					<li>{{$cookies->choice->li2}}</li>
-					<li>{{$cookies->choice->li3}}</li>
-					<li>{{$cookies->choice->li4}}</li>
-					<li>{{$cookies->choice->li5}}</li>
-				</ol>
-				
-				
-			</p>
-			<p>
-				<h2>{{$cookies->moreinfo->h}}</h2>
-				<p>
-					{{$cookies->moreinfo->body}}
+					<h2>{{$cookies->whararecookies->h}}</h2>
+					{{$cookies->whararecookies->div1}}
+					<p>{{$cookies->whararecookies->div2}}</p>
 				</p>
 				<p>
-					{{$cookies->moreinfo->body->subb->h}}
+					<h2>{{$cookies->howweuse->h}}</h2>
+					{{$cookies->howweuse->div1}}
+					<ol>
+						{{$cookies->howweuse->subh}}
+						<li>{{$cookies->howweuse->li1}}</li>
+						<li>{{$cookies->howweuse->li2}}</li>
+						<li>{{$cookies->howweuse->li3}}</li>
+					</ol>
+				</p>
+				<p>
+					<h2>{{$cookies->choice->h}}</h2>
 					<p>
-						<a href="{{$cookies->moreinfo->body->subb->li1}}" _blank>
-							{{$cookies->moreinfo->body->subb->li1}}
-						</a>
-						
+						{{$cookies->choice->div1}}
 					</p>
 					<p>
-						<a href="{{$cookies->moreinfo->body->subb->li2}}" _blank>
-							{{$cookies->moreinfo->body->subb->li2}}
-						</a>
-						
+						{{$cookies->choice->div2}}
 					</p>
+					<p>
+						{{$cookies->choice->subh}}
+					</p>
+					<ol>
+						<li>{{$cookies->choice->li1}}</li>
+						<li>{{$cookies->choice->li2}}</li>
+						<li>{{$cookies->choice->li3}}</li>
+						<li>{{$cookies->choice->li4}}</li>
+						<li>{{$cookies->choice->li5}}</li>
+					</ol>
+					
 					
 				</p>
-					
-			</p>
+				<p>
+					<h2>{{$cookies->moreinfo->h}}</h2>
+					<p>
+						{{$cookies->moreinfo->body}}
+					</p>
+					<p>
+						{{$cookies->moreinfo->body->subb->h}}
+						<p>
+							<a href="{{$cookies->moreinfo->body->subb->li1}}" _blank>
+								{{$cookies->moreinfo->body->subb->li1}}
+							</a>
+							
+						</p>
+						<p>
+							<a href="{{$cookies->moreinfo->body->subb->li2}}" _blank>
+								{{$cookies->moreinfo->body->subb->li2}}
+							</a>
+							
+						</p>
+						
+					</p>
+						
+				</p>
 
+			</div>
 		</div>
+		<div class="col-md-2 col-lg-2"></div>
 	</div>
-	<div class="col-md-2 col-lg-2"></div>
 </div>
+	
 		
 <style type="text/css">
 	.navbarDiv {
--- resources/views/privacy/privacy.blade.php
+++ resources/views/privacy/privacy.blade.php
@@ -7,66 +7,70 @@
 
 @section ("content")
 
-<div class="row">
-	<div class="col-md-2 col-lg-2"></div>
-	<div class="col-md-8 col-lg-8">
-		<div class="row">
 
-			@php
+<div class="container" style="font-size:Times New Roman">
+	<div class="row">
+		<div class="col-md-2 col-lg-2"></div>
+		<div class="col-md-8 col-lg-8">
+			<div class="row">
 
-			$cookies = simplexml_load_file("xml/privacy.xml");
-			@endphp
+				@php
 
-			<h1>
-				{{$cookies->header}}
-			</h1>
-			<h3>{{$cookies->updatetime}}</h3>
-			<p>
-				{{$cookies->topbody->div1}}<br>
-			</p>
-			<p>
-				{{$cookies->topbody->div2}}
-			</p>
-			<p>
-				<h2>{{$cookies->whararecookies->h}}</h2>
-				{{$cookies->whararecookies->div1}}
-				<p>{{$cookies->whararecookies->div2}}</p>
-			</p>
-			<p>
-				<h2>{{$cookies->howweuse->h}}</h2>
-				{{$cookies->howweuse->div1}}
+				$cookies = simplexml_load_file("xml/privacy.xml");
+				@endphp
+
+				<h1>
+					{{$cookies->header}}
+				</h1>
+				<h3>{{$cookies->updatetime}}</h3>
 				<p>
-					{{$cookies->howweuse->div2}}<a href="{{env('APP_URL')}}cookiespolicy">{{$cookies->howweuse->div2->link}}</a>
+					{{$cookies->topbody->div1}}<br>
 				</p>
-			</p>
-			<p>
-				<h2>{{$cookies->choice->h}}</h2>
 				<p>
-					{{$cookies->choice->div1}}
+					{{$cookies->topbody->div2}}
 				</p>
-				
-				
-			</p>
-			<p>
-				<h2>{{$cookies->consent->h}}</h2>
 				<p>
-					{{$cookies->consent->div1}}
+					<h2>{{$cookies->whararecookies->h}}</h2>
+					{{$cookies->whararecookies->div1}}
+					<p>{{$cookies->whararecookies->div2}}</p>
 				</p>
-				
-				
-			</p>
-			<p>
-				<h2>{{$cookies->moreinfo->h}}</h2>
 				<p>
-					{{$cookies->moreinfo->body}}
+					<h2>{{$cookies->howweuse->h}}</h2>
+					{{$cookies->howweuse->div1}}
+					<p>
+						{{$cookies->howweuse->div2}}<a href="{{env('APP_URL')}}cookiespolicy">{{$cookies->howweuse->div2->link}}</a>
+					</p>
 				</p>
+				<p>
+					<h2>{{$cookies->choice->h}}</h2>
+					<p>
+						{{$cookies->choice->div1}}
+					</p>
 					
-			</p>
+					
+				</p>
+				<p>
+					<h2>{{$cookies->consent->h}}</h2>
+					<p>
+						{{$cookies->consent->div1}}
+					</p>
+					
+					
+				</p>
+				<p>
+					<h2>{{$cookies->moreinfo->h}}</h2>
+					<p>
+						{{$cookies->moreinfo->body}}
+					</p>
+						
+				</p>
 
+			</div>
 		</div>
+		<div class="col-md-2 col-lg-2"></div>
 	</div>
-	<div class="col-md-2 col-lg-2"></div>
 </div>
+	
 		
 <style type="text/css">
 	.navbarDiv {
