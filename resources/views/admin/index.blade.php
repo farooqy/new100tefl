ff7e637410dea796be637ac7ad86ec999b234730
farooqy Desktop <noorfarooqy@gmail.com>
2019-02-15 12:00:48 +0800

    Added icons for the adminstrative tools

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,18 +11,41 @@
   height: 120px;
 }
 </style>
-<div class="container">
-	<div class="row">
+<div class="container" style="font-family: Times New Roman">
+	<div class="row text-center">
 		<h2>Administrtive tool </h2>
 		{{session('logStatus')}}
 	</div>
+	<div class="row">
+		<div class="col-md-3 col-lg-3">
+			<div class="row text-center">
+				<img src="{{env('APP_URL')}}images/uploads/icons/newsicon.jpg" height="200px" width="100%" style="border:thin solid gray; border-radius: 4px">
+			</div>
+			<div class="row">
+				<a href="news/addnews" style="font-size: 18px">Add news</a>
+			</div>
+		</div>
+		<div class="col-md-3 col-lg-3">
+			<div class="row">
+				<img src="{{env('APP_URL')}}images/uploads/icons/feedback.jpg" height="200px" width="100%" style="border:thin solid gray; border-radius: 4px">
+			</div>
+			<div class="row">
+				<a href="admins/feedback" style="font-size: 18px">Client Feedbacks</a>
+			</div>
+		</div>
+		<div class="col-md-3 col-lg-3">
+			<div class="row">
+				<img src="{{env('APP_URL')}}images/uploads/icons/membersicon.png" height="200px" width="100%"  style="border:thin solid gray; border-radius: 4px">
+			</div>
+			<div class="row">
+				<a href="admins/feedback" style="font-size: 18px">Member Details</a>
+			</div>
+		</div>
+	</div>
+			
 
-	<div class="row">
-		<a href="admins/feedback">Client Feedbacks</a>
-	</div>
-	<div class="row">
-		<a href="news/addnews">Add news</a>
-	</div>
+			
+			
 </div>
 
 @endsection
